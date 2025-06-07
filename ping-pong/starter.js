58c82f109edc3a4c692856b155d3ac12a8c5ee96
SebastianSotoAr <ssotoa@unal.edu.co>
2025-06-07 18:26:02 -0500

    Ejercicios-sin-resolver

---
+++ ping-pong/starter.js
@@ -0,0 +1,8 @@
+//1. Acciones a los botones
+//2. Botón verde asociado a jugador 1
+//3. Botón rojo asociado a jugador 2
+//4. Botón de reset
+//5. Select de cantidad de puntos a jugar
+//6. Lógica de juego
+
+// Aquí comienza tu código
